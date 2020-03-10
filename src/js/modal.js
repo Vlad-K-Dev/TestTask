d54c3dab138ba27aaa66f6ee13d3de017a29fce8
vlad.k <Vladislav K>
2020-03-10 06:47:05 +0300

    try fix modal width and add listner on numeric menu

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
+        const numMenu = document.querySelectorAll('.promo-block__list');
         const linkBtn = document.querySelector('#js-hyperlink');
         const modalClose = document.querySelector('#js-modalClose');
         const modal = document.querySelector('#js-modal');
         const overlay = document.querySelector('#js-overlay');
-        linkBtn.addEventListener('click', function () {
-                overlay.style.display = 'block';
-                modal.style.display = 'block';
-        })
-        modalClose.addEventListener('click', function () {
-                overlay.style.display = 'none';
-                modal.style.display = 'none';
-        })
-
-
-
+        for(let i = 0; i < numMenu.length; i++){
+                numMenu[i].addEventListener('click', function () {
+                        overlay.style.display = 'block';
+                        modal.style.display = 'block';
+                        modal.style.width = '18.75rem';
+                });
+                modalClose.addEventListener('click', function () {
+                        overlay.style.display = 'none';
+                        modal.style.display = 'none';
+                })
+        }
 });
+
+
